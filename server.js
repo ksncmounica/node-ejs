d7630e5d89264e79d4dc74abf9555e61c4c3bd79
ksncmounica <mounica.kokkerlapati@mtuity.com>
2016-05-19 17:21:59 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+// load the things we need
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page 
+app.get('/', function(req, res) {
+	res.render('pages/index');
+});
+
+// about page 
+app.get('/about', function(req, res) {
+	res.render('pages/about');
+});
+
+app.listen(3000);
+console.log('3000 is the magic port');
+
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
